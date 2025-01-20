84cbaeee755608caea118ccb586b46cf72920a8a
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-20 09:56:47 +0530

    Revert "Added CI/CD workflow using AWS CodePipeline"

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# pulling docker images
-docker pull amitabhdevops/aws-node-todo-app-cicd:v1
-
-# Run the Docker image as a container
-docker run -d -p 8000:8000 amitabhdevops/aws-node-todo-app-cicd:v1
